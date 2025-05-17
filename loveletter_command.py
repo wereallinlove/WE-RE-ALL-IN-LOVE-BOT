66fd8aa5f2e70e86f860a6396e897b1f0bc972c5
wereallinlove <wereallinlove27@gmail.com>
2025-05-16 23:58:13 -0400

    Update loveletter_command.py

--- loveletter_command.py
+++ loveletter_command.py
@@ -1,35 +1,38 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
+from discord import app_commands
 import random
 
-LOVELY_IMAGES = [
-    "https://media.tenor.com/14udFQfQ7NQAAAAC/pixel-hearts.gif",
-    "https://media.tenor.com/yheo1GGu3FwAAAAC/heart.gif",
-    "https://media.tenor.com/3N0aUzZ9c38AAAAC/hearts.gif"
-]
-
-class Loveletter(commands.Cog):
+class LoveletterCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="loveletter", description="Send an anonymous love letter to someone")
-    @app_commands.describe(user="The person you want to send it to", message="The love letter message")
+    @app_commands.command(name="loveletter", description="Send an anonymous love letter to someone.")
+    @app_commands.describe(user="The person you want to send it to", message="Your anonymous message to them")
     async def loveletter(self, interaction: discord.Interaction, user: discord.User, message: str):
+        love_channel = self.bot.get_channel(1372782806446506047)
+        if not love_channel:
+            await interaction.response.send_message("Love letter channel not found.", ephemeral=True)
+            return
+
         embed = discord.Embed(
             title="💌 Love Letter",
             description=f"*{message}*\n\nTo: {user.mention}",
             color=discord.Color.pink()
         )
-        embed.set_image(url=random.choice(LOVELY_IMAGES))
+
+        love_gifs = [
+            "https://media.tenor.com/14udFQfQ7NQAAAAC/pixel-hearts.gif",
+            "https://media.tenor.com/yheo1GGu3FwAAAAC/heart.gif",
+            "https://media.tenor.com/3N0aUzZ9c38AAAAC/hearts.gif",
+            "https://media.tenor.com/xUa0jz2NS9kAAAAC/pink-love.gif",
+            "https://media.tenor.com/_4YgA77ExHEAAAAC/love.gif"
+        ]
+        embed.set_image(url=random.choice(love_gifs))
         embed.set_footer(text=f"WE'RE ALL IN LOVE {discord.utils.utcnow().year}")
 
-        channel = self.bot.get_channel(1372782806446506047)
-        if channel:
-            await channel.send(embed=embed)
-            await interaction.response.send_message("Your love letter was sent anonymously! 😍", ephemeral=True)
-        else:
-            await interaction.response.send_message("Failed to find the love letter channel.", ephemeral=True)
+        await love_channel.send(embed=embed)
+        await interaction.response.send_message("Your love letter was sent anonymously 💘", ephemeral=True)
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Loveletter(bot))
+    await bot.add_cog(LoveletterCommand(bot))
